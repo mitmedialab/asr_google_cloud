01eeb95924e04751b81eb6cdc62cee946cebb100
jakory <jakory@media.mit.edu>
2018-02-03 11:32:24 -0500

    Update run script to use rospack to find this package

--- run_ros_asr.sh
+++ run_ros_asr.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Jacqueline Kory Westlund
 # January 2018
-cd "$HOME/projects/ros_catkin_ws/src/asr_google_cloud/"
+cd "$(rospack find asr_google_cloud)"
 pipenv run python src/ros_asr.py
