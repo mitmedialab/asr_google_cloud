bb3810ca15c8dcd44b20ac6665c330fbeac330e3
Hae Won Park <haewon@mit.edu>
2019-04-07 19:09:22 -0400

    local mic option

--- src/local_mic_asr.py
+++ src/local_mic_asr.py
@@ -1,23 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
-Jacqueline Kory Westlund
-August 2017
-
-This code is adapted from the Google Cloud Platform python examples, which say:
-Copyright 2017 Google Inc. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License.  You may obtain a copy of the
-License at
-     http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations under the License.
-
+Hae Won Park
+April 2019
 
 The MIT License (MIT)
 
-Copyright (c) 2017 Personal Robots Group
+Copyright (c) 2019 Personal Robots Group
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -37,150 +25,298 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+Google Cloud Speech API sample application using the streaming API.
+
+NOTE: This module requires the additional dependency `pyaudio`. To install
+using pip:
+
+    pip install pyaudio
+
+Example usage:
+    python transcribe_streaming_indefinite.py
 """
 
-import argparse  # Get command-line arguments.
-import pyaudio  # Get audio from local microphone.
-from six.moves import queue  # Thread-safe queue for buffering audio data.
-from google.cloud import speech as google_speech
-from google.cloud.speech import enums as google_enums
-from google.cloud.speech import types as google_types
+# [START speech_transcribe_infinite_streaming]
+from __future__ import division
+
+import time
+import re
+import sys
+
+from google.cloud import speech
+
+import pyaudio
+from six.moves import queue
+
+# ROS setup
+import rospy  # Get audio data over ROS and publish results.
+from std_msgs.msg import Header
+from asr_google_cloud.msg import AsrResult
+from asr_google_cloud.msg import AsrCommand
+
+# Audio recording parameters
+STREAMING_LIMIT = 55000
+SAMPLE_RATE = 16000
+CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
 
 
-class LocalMicStream(object):
-    """ Open an audio stream from a local microphone as a generator that gives
-    us chunks of audio data.
-    """
-    def __init__(self, sample_rate, buffer_size):
-        # Keep the provided sample rate and buffer size for setting up our
-        # audio stream later.
-        self._sample_rate = sample_rate
-        self._buffer_size = buffer_size
-        # Thread-safe buffer of audio data.
-        self._buffer = queue.Queue()
-        # The audio stream starts out closed.
+def get_current_time():
+    return int(round(time.time() * 1000))
+
+
+def duration_to_secs(duration):
+    return duration.seconds + (duration.nanos / float(1e9))
+
+
+class ResumableMicrophoneStream:
+    """Opens a recording stream as a generator yielding the audio chunks."""
+    def __init__(self, rate, chunk_size):
+        self._rate = rate
+        self._chunk_size = chunk_size
+        self._num_channels = 1
+        self._max_replay_secs = 5
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
         self.closed = True
+        self.start_time = get_current_time()
+
+        # 2 bytes in 16 bit samples
+        self._bytes_per_sample = 2 * self._num_channels
+        self._bytes_per_second = self._rate * self._bytes_per_sample
+
+        self._bytes_per_chunk = (self._chunk_size * self._bytes_per_sample)
+        self._chunks_per_second = (
+                self._bytes_per_second // self._bytes_per_chunk)
 
     def __enter__(self):
-        """ Set up the audio stream. """
-        # Initialize pyaudio.
-        self._pyaud = pyaudio.PyAudio()
-
-        # Open stream. The Google Cloud Speech API only supports 1-channel
-        # (i.e. mono) audio, so we don't make that a tunable parameter. We
-        # collect audio from the stream into a buffer asynchronously with a
-        # callback.
-        self._stream = self._pyaud.open(
-            format=pyaudio.paInt16,
-            channels=1,
-            rate=self._sample_rate,
-            input=True,
-            frames_per_buffer=self._buffer_size,
-            stream_callback=self._fill_buffer)
-        # The stream is now open.
         self.closed = False
+
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=self._num_channels,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk_size,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
         return self
 
-    def __exit__(self, type_, value, traceback):
-        """ Make sure we stop and close the audio stream when we exit. """
-        self._stream.stop_stream()
-        self._stream.close()
-        # Set the closed flag so the generator will stop looping.
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
         self.closed = True
-        # Tell the generator to terminate by adding None to the buffer.
-        self._buffer.put(None)
-        # Terminate pyaudio too.
-        self._pyaud.terminate()
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
 
-    def _fill_buffer(self, data, frames, time, status_flags):
-        """ Collect audio data from the stream and put it in our buffer. """
-        self._buffer.put(data)
+    def _fill_buffer(self, in_data, *args, **kwargs):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
         return None, pyaudio.paContinue
 
-    def audio_generator(self):
-        """ Get data from the buffer when requested. """
+    def generator(self):
         while not self.closed:
-            # Block on get() until there's data. That way, we get at least one
-            # chunk of data each time.
-            data_chunk = self._buffer.get()
-            # Stop iterating if we just got a None in the buffer (our signal
-            # that we're at the end of the audio stream).
-            if data_chunk is None:
+            if get_current_time() - self.start_time > STREAMING_LIMIT:
+                self.start_time = get_current_time()
+                break
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
                 return
-            # Make a list of data chunks. We'll add more chunks later.
-            data = [data_chunk]
+            data = [chunk]
 
-            # Consume any additional data from the buffer.
+            # Now consume whatever other data's still buffered.
             while True:
                 try:
-                    # Get more audio. In this case, we don't need to block
-                    # because we know we have at least some data.
-                    data_chunk = self._buffer.get(block=False)
-                    # Again, if we get None, we got our signal that we're at
-                    # the end of the stream, so stop collecting audio.
-                    if data_chunk is None:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
                         return
-                    # Otherwise, collect the audio into our data list.
-                    data.append(data_chunk)
-                # Stop collecting data if the queue is empty.
+                    data.append(chunk)
                 except queue.Empty:
                     break
 
-            # Return all the audio data collected so far as a byte array.
-            yield b"".join(data)
+            if not send_data:
+                return
+
+            yield b''.join(data)
 
 
-def handle_responses(responses):
-    """ Iterate through any responses received. """
+def listen_print_loop(responses, stream):
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    responses = (r for r in responses if (
+            r.results and r.results[0].alternatives))
+
+    num_chars_printed = 0
     for response in responses:
         if not response.results:
             continue
-        print response
 
+        # Send results over ROS.
+        msg = AsrResult()
+        msg.header = Header()
+        msg.header.stamp = rospy.Time.now()
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        top_alternative = result.alternatives[0]
+        transcript = top_alternative.transcript
+
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + '\r')
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+        else:
+            print(transcript + overwrite_chars)
+
+            #print("Got final result:\n{}".format(response))
+            # TODO publish alternatives
+            if publish_alternatives:
+                print("TODO publish alternatives")
+            msg.transcription = str(response.results[0].alternatives[0].
+                                    transcript)
+            msg.confidence = response.results[0].alternatives[0].confidence
+            pub_asr_result.publish(msg)
+            print("*** SENT RESULT ***")
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            # if re.search(r'\b(exit|quit)\b', transcript, re.I):
+            #     print('Exiting..')
+            #     stream.closed = True
+            #     break
+
+            num_chars_printed = 0
+
+def on_asr_command(data):
+    """ Receive and process a command message telling this node to start or
+    stop streaming audio to Google.
+    """
+    print("Received ASR command: {}".format(data.command))
+    print("ASR COMMAND RECEIVED **********************")
+    global publish_final, publish_interim, publish_alternatives, send_data
+    # Should we stop streaming data to Google for processing or stop sending
+    # any kind of results back? If no results streaming is enabled, we won't
+    # send anything to Google.
+    if (data.command == AsrCommand.STOP_ALL or
+            data.command == AsrCommand.STOP_FINAL):
+        # Stop streaming final results.
+        publish_final = False
+    if (data.command == AsrCommand.STOP_ALL or
+            data.command == AsrCommand.STOP_ALTERNATIVES):
+        # Stop streaming alternative results.
+        publish_alternatives = False
+    if (data.command == AsrCommand.STOP_ALL or
+            data.command == AsrCommand.STOP_INTERIM):
+        # Stop streaming interim results.
+        publish_interim = False
+
+    # Or should we start streaming data to Google for processing or start
+    # sending a particular kind of results?
+    if (data.command == AsrCommand.START_ALL or
+            data.command == AsrCommand.START_FINAL):
+        # Start streaming final results.
+        publish_final = True
+    if (data.command == AsrCommand.START_ALL or
+            data.command == AsrCommand.START_ALTERNATIVES):
+        # Start streaming alternative results.
+        publish_alternatives = True
+    if (data.command == AsrCommand.START_ALL or
+            data.command == AsrCommand.START_INTERIM):
+        # Start streaming interim results.
+        publish_interim = True
+
+    # If we should now be publishing results, start ASR.
+    if publish_final or publish_alternatives or publish_interim:
+        send_data = True
+    else:
+        send_data = False
 
 def main():
-    """ Run Google ASR with the local microphone stream. """
 
-    parser = argparse.ArgumentParser(
-        description=""" Use the Google Cloud Speech API to get Google ASR
-        results for audio streamed from the local microphone. """)
-    parser.add_argument("-r, --sample_rate", type=int, nargs='?',
-                        dest="sample_rate", action='store', default=16000,
-                        help="""Sample rate at which audio will be streamed.
-                        Defaults to 16000.""")
-    parser.add_argument("-b, --buffer_size", type=int, nargs='?',
-                        dest="buffer_size", action='store', default=1600,
-                        help="""Buffer size (also called chunk size) for audio
-                        being collected. Defaults to 1600 (i.e., one tenth of
-                        the default sample rate, or 100ms).""")
-    # Get arguments.
-    args = parser.parse_args()
-    print "Got arguments: {}".format(args)
+    rospy.init_node('google_asr_node', anonymous=False)
 
-    # Set up client to talk to Google.
-    language_code = 'en-US'
-    client = google_speech.SpeechClient()
-    # The audio encoding arg curently specifies raw 16-bit signed LE samples.
-    config = google_types.RecognitionConfig(
-        encoding=google_enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=args.sample_rate,
-        language_code=language_code)
-    # TODO add as arg above: speech_context={"phrases": ["words", "here"]}
-    streaming_config = google_types.StreamingRecognitionConfig(
-        config=config, interim_results=True)
+    # Publish ASR results as asr_google_cloud/AsrResult messages.
+    global pub_asr_result
+    pub_asr_result = rospy.Publisher('asr_result', AsrResult, queue_size=10)
 
-    # Start streaming audio to Google.
-    with LocalMicStream(args.sample_rate, args.buffer_size) as stream:
-        audio_stream_generator = stream.audio_generator()
-        requests = (google_types.StreamingRecognizeRequest(
-            audio_content=content) for content in audio_stream_generator)
+    # Subscribe to basic commands to tell this node to start or stop processing
+    # audio and streaming to Google, as well as what results to publish.
+    global sub_asr_command
+    sub_asr_command = rospy.Subscriber('asr_command', AsrCommand, on_asr_command)
 
-        # Get responses from Google.
-        responses = client.streaming_recognize(streaming_config, requests)
+    global publish_interim
+    publish_interim = False
+    global publish_alternatives
+    publish_alternatives = False
+    global publish_final
+    publish_final = True
+    global send_data
+    send_data = True
 
-        # Use the ASR responses.
-        handle_responses(responses)
+    client = speech.SpeechClient()
+    config = speech.types.RecognitionConfig(
+        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=SAMPLE_RATE,
+        language_code='en-US',
+        max_alternatives=1,
+        enable_word_time_offsets=True)
+    streaming_config = speech.types.StreamingRecognitionConfig(
+        config=config,
+        interim_results=True)
+
+    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
+
+    #print('Say "Quit" or "Exit" to terminate the program.')
+
+    with mic_manager as stream:
+        while not stream.closed:
+            audio_generator = stream.generator()
+            requests = (speech.types.StreamingRecognizeRequest(
+                audio_content=content)
+                for content in audio_generator)
+
+            responses = client.streaming_recognize(streaming_config,
+                                                   requests)
+            # Now, put the transcription responses to use.
+            listen_print_loop(responses, stream)
 
 
 if __name__ == '__main__':
     main()
+# [END speech_transcribe_infinite_streaming]
