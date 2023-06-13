27ba3ed728305d294848bb06b9d89dc727f41acd
Audrey Lee <audrey16@mit.edu>
2023-06-13 15:36:16 -0400

    Back to attempt 3

--- src/local_mic_asr copy.py
+++ src/local_mic_asr copy.py
@@ -47,7 +47,6 @@ import sys
 import websockets
 import asyncio
 import base64
-import websocket
 
 # from google.cloud import speech
 import assemblyai as aai
@@ -352,44 +351,6 @@ async def send_receive(recorder: ResumableMicrophoneStream):
         
         send_result, receive_result = await asyncio.gather(send(), receive())
 
-def on_message(ws, message):
-    message = json.loads(message)
-    if message["message_type"] == "SessionBegins":
-        session_id = message["session_id"]
-        expires_at = message["expires_at"]
-        print(f"Session ID: {session_id}")
-        print(f"Expires at: {expires_at}")
-    elif message["message_type"] == "PartialTranscript":
-        print(f"Partial transcript received: {message['text']}")
-    elif message['message_type'] == 'FinalTranscript':
-        print(f"Final transcript received: {message['text']}")
-
-def on_error(ws, error):
-    error_message = json.loads(error)
-    if error_message['error_code'] == 4000:
-        print("Sample rate must be a positive integer")
-
-def on_close(ws):
-    print("WebSocket closed")
-
-def on_open(ws):
-    print("WebSocket opened")
-    auth_header = {"Authorization": ASSEMBLYAI_API_KEY}
-    sample_rate = SAMPLE_RATE
-    ws.send(f"wss://api.assemblyai.com/v2/realtime/ws?sample_rate={sample_rate}", header=auth_header)
-
-def send_audio(ws, audio_data):
-    payload = {
-        "audio_data": base64.b64encode(audio_data).decode("utf-8")
-    }
-    ws.send(json.dumps(payload))
-
-def terminate_session(socket):
-    payload = {'terminate_session': True}
-    message = json.dumps(payload)
-    socket.send(message)
-    socket.close()
-
 def main():
 
     rospy.init_node('google_asr_node', anonymous=False)
@@ -414,15 +375,10 @@ def main():
 
     mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
     
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://api.assemblyai.com/v2/realtime/ws", on_message=on_message, on_error=on_error, on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
-        
-    # #print('Say "Quit" or "Exit" to terminate the program.')
-    # with mic_manager as stream:
-    #     while not stream.closed:
-    #         asyncio.run(send_receive(stream))
+    #print('Say "Quit" or "Exit" to terminate the program.')
+    with mic_manager as stream:
+        while not stream.closed:
+            asyncio.run(send_receive(stream))
 
 if __name__ == '__main__':
     main()
