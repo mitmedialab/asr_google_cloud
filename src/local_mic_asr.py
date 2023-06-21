7559b9c7fb4c630359e39ca88cb66c4d23784933
Jocelyn Shen <jocelyn.j.shen@gmail.com>
2023-06-20 18:59:07 -0700

    trying to address latency

--- src/local_mic_asr.py
+++ src/local_mic_asr.py
@@ -1,42 +1,3 @@
-#!/usr/bin/env python
-"""
-Hae Won Park
-April 2019
-
-The MIT License (MIT)
-
-Copyright (c) 2019 Personal Robots Group
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-Google Cloud Speech API sample application using the streaming API.
-
-NOTE: This module requires the additional dependency `pyaudio`. To install
-using pip:
-
-    pip install pyaudio
-
-Example usage:
-    python transcribe_streaming_indefinite.py
-"""
-
-# [START speech_transcribe_infinite_streaming]
 from __future__ import division
 
 import json
@@ -120,12 +81,7 @@ class ResumableMicrophoneStream:
             rate=self._rate,
             input=True,
             frames_per_buffer=self._chunk_size,
-            input_device_index=self._input_device_index,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
-            # NOTE uncomment to run google cloud asr
-            # stream_callback=self._fill_buffer,
+            input_device_index=self._input_device_index
         )
 
         return self
@@ -143,119 +99,6 @@ class ResumableMicrophoneStream:
         """Continuously collect data from the audio stream, into the buffer."""
         self._buff.put(in_data)
         return None, pyaudio.paContinue
-
-    def generator(self):
-        while not self.closed:
-            if get_current_time() - self.start_time > STREAMING_LIMIT:
-                self.start_time = get_current_time()
-                break
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
-            chunk = self._buff.get()
-            if chunk is None:
-                return
-            data = [chunk]
-
-            # Now consume whatever other data's still buffered.
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                except queue.Empty:
-                    break
-
-            if not send_data:
-                return
-
-            yield b''.join(data)
-
-
-def listen_print_loop(responses, stream):
-    """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
-    responses = (r for r in responses if (
-            r.results and r.results[0].alternatives))
-
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
-
-        # Send results over ROS.
-        msg = AsrResult()
-        msg.header = Header()
-        msg.header.stamp = rospy.Time.now()
-
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
-        result = response.results[0]
-        if not result.alternatives:
-            continue
-
-        # Display the transcription of the top alternative.
-        top_alternative = result.alternatives[0]
-        transcript = top_alternative.transcript
-
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
-
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + '\r')
-            sys.stdout.flush()
-
-            num_chars_printed = len(transcript)
-        else:
-            print(transcript + overwrite_chars)
-
-            # print("Got final result:\n{}".format(response))
-            # TODO publish alternatives
-            if publish_alternatives:
-                print("TODO publish alternatives")
-            msg.transcription = str(response.results[0].alternatives[0].
-                                    transcript)
-            msg.confidence = response.results[0].alternatives[0].confidence
-
-            for i in range(len(response.results[0].alternatives[0].words)):
-                w = Words()
-                w.word = response.results[0].alternatives[0].words[i].word
-                w.start_time = response.results[0].alternatives[0].words[i].start_time.seconds + \
-                               float(response.results[0].alternatives[0].words[i].start_time.microseconds) * 1e-6
-                w.end_time = response.results[0].alternatives[0].words[i].end_time.seconds + \
-                             float(response.results[0].alternatives[0].words[i].end_time.microseconds) * 1e-6
-
-                msg.words_list.append(w)
-
-            pub_asr_result.publish(msg)
-            print("*** SENT RESULT ***")
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            # if re.search(r'\b(exit|quit)\b', transcript, re.I):
-            #     print('Exiting..')
-            #     stream.closed = True
-            #     break
-
-            num_chars_printed = 0
 
 def on_asr_command(data):
     """ Receive and process a command message telling this node to start or
@@ -330,8 +173,6 @@ async def send_receive(recorder: ResumableMicrophoneStream):
                         # print(e)
                         assert False, "Not a websocket 4008 error"
                     await asyncio.sleep(0.01)
-                else:
-                    await asyncio.sleep(2)
         
         async def receive():
             while True:
@@ -339,7 +180,7 @@ async def send_receive(recorder: ResumableMicrophoneStream):
                     result_str = await _ws.recv()
                     result = json.loads(result_str)
                     if (result['message_type']=="FinalTranscript" and result['text']):
-                        print(result['text'])
+                        print("FINAL: ", result['text'])
                         msg = AsrResult()
                         msg.header = Header()
                         msg.header.stamp = rospy.Time.now()
@@ -356,7 +197,19 @@ async def send_receive(recorder: ResumableMicrophoneStream):
                     elif (result['message_type']=='SessionBegins'):
                         print(result)
                     elif (result['message_type']=='PartialTranscript' and result['text']):
-                        # print(result['text']) # for debugging
+                        msg = AsrResult()
+                        msg.header = Header()
+                        msg.header.stamp = rospy.Time.now()
+                        msg.transcription = result['text']
+                        msg.confidence = result['confidence']
+                        for i in result['words']:
+                            w = Words()
+                            w.word = i['text']
+                            w.start_time = i['start']
+                            w.end_time = i['end']
+                            msg.words_list.append(w)
+                        pub_asr_result.publish(msg)
+                        print("**", result['text']) # for debugging
                         pass
                 except websockets.exceptions.ConnectionClosedError as e:
                     # print(e)
@@ -392,37 +245,6 @@ def main():
     
     mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
 
-    ### Google Cloud Version
-    '''
-    client = speech.SpeechClient()
-    config = speech.RecognitionConfig(
-        encoding="LINEAR16",
-        sample_rate_hertz=SAMPLE_RATE,
-        language_code='en-US',
-        enable_word_time_offsets=True)
-    streaming_config = speech.StreamingRecognitionConfig(
-        config=config,
-        interim_results=True)
-
-    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
-
-    #print('Say "Quit" or "Exit" to terminate the program.')
-
-    with mic_manager as stream:
-        while not stream.closed:
-            audio_generator = stream.generator()
-            requests = (speech.StreamingRecognizeRequest(
-                audio_content=content)
-                for content in audio_generator)
-
-            responses = client.streaming_recognize(streaming_config,
-                                                   requests)
-            # Now, put the transcription responses to use.
-            listen_print_loop(responses, stream)
-    '''
-    
-    ### Assembly AI (with asyncio) version
-    # '''
     while True:
         if True:
             if mic_manager.closed:
@@ -440,4 +262,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-# [END speech_transcribe_infinite_streaming]
---
+++ src/test.py
@@ -0,0 +1,117 @@
+import pyaudio
+ 
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+p = pyaudio.PyAudio()
+ 
+# starts recording
+stream = p.open(
+   format=FORMAT,
+   channels=CHANNELS,
+   rate=RATE,
+   input=True,
+   frames_per_buffer=FRAMES_PER_BUFFER,
+   input_device_index=6
+)
+
+import websockets
+import asyncio
+import base64
+import json
+from passwords import ASSEMBLYAI_API_KEY
+run = False
+ 
+"""
+Make sure to install the following requirements:
+
+pip install -U websocket-client
+
+additional, Mac users may need to execute:
+
+brew install portaudio
+"""
+
+import websocket
+import base64
+import pyaudio
+import json
+from threading import Thread
+
+
+YOUR_API_TOKEN = "YOUR-API-TOKEN"
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+SAMPLE_RATE = 16000
+p = pyaudio.PyAudio()
+
+def on_message(ws, message):
+    """
+    is being called on every message
+    """
+    transcript = json.loads(message)
+    text = transcript['text']
+
+    if transcript["message_type"] == "PartialTranscript":
+        print(f"Partial transcript received: {text}")
+    elif transcript['message_type'] == 'FinalTranscript':
+        print(f"Final transcript received: {text}")
+
+
+def on_open(ws):
+    """
+    is being called on session begin
+    """
+    def send_data():
+        while True:
+            # read from the microphone
+            data = stream.read(FRAMES_PER_BUFFER)
+
+            # encode the raw data into base64 to send it over the websocket
+            data = base64.b64encode(data).decode("utf-8")
+
+            # Follow the message format of the Real-Time service (see documentation)
+            json_data = json.dumps({"audio_data":str(data)})
+
+
+            # Send the data over the wire
+            ws.send(json_data)
+
+
+    # Start a thread where we send data to avoid blocking the 'read' thread
+    Thread(target=send_data).start()
+    
+def on_error(ws, error):
+    """
+    is being called in case of errors
+    """
+    print(error)
+
+
+def on_close(ws):
+    """
+    is being called on session end
+    """
+    print("WebSocket closed")
+
+
+# Set up the WebSocket connection with your desired callback functions
+websocket.enableTrace(False)
+
+# After opening the WebSocket connection, send an authentication header with your API token
+auth_header = {"Authorization": f"{ASSEMBLYAI_API_KEY}" }
+
+ws = websocket.WebSocketApp(
+    f"wss://api.assemblyai.com/v2/realtime/ws?sample_rate={SAMPLE_RATE}",
+    header=auth_header,
+    on_message=on_message,
+    on_open=on_open,
+    on_error=on_error,
+    on_close=on_close
+)
+
+
+# Start the WebSocket connection
+ws.run_forever()
